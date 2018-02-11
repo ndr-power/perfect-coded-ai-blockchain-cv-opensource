c55105b7a75f12e2dd78de3ebfab06dc49639502
ndr-power <ndrpowerr@gmail.com>
2018-02-11 15:11:16 +0300

    update haha.js hehe

--- haha.js
+++ haha.js
@@ -17,30 +17,34 @@ fs.readFile('Camera_Out','utf8', (err, file) => {
         }
     })
     data = rotate2d(data)
-    data = rotate2d(data)
+    // data = rotate2d(data)
     
     let topx = [-1, 0],
     topy = [0, -1],
     lowx = [1500000, 0],
     lowy = [0,1500000]
-
+    let lefttop = [0,1555]
+    let leftbottom = []
+    let righttop = []
+    let rightbottom = []
+    topx = getTopLeft(data)
+    topy = getTopRight(data)
+    lowx = getBotLeft(data)
+    lowy = getBotRight(data)
     
-//     for(let x = 0; x<data.length;x++){
-//         for(let y = 0; y<data[0].length; y++){
-//             if (data[x][y] == 255){
-                
-//                 if (topx[0] < x) topx = [x,y]
-//                 if (topy[1] < y) topy = [x,y]
-//                 if (lowx[0] > x) lowx = [x,y]
-//                 if (lowy[1] > y) lowy = [x,y]
-            
-//             image.setAt(x,y, { red:255, green:0, blue:5, alpha:1000 })
-//         }else{
-//             image.setAt(x,y, { red:0, green:0, blue:0, alpha:1000 })
-//             }
+
+    data = rotate2d(data)
+    
+    for(let x = 0; x<data.length;x++){
+        for(let y = 0; y<data[0].length; y++){
+            if (data[x][y] == 255){
+            image.setAt(x,y, { red:255, green:0, blue:5, alpha:1000 })
+        }else{
+            image.setAt(x,y, { red:0, green:0, blue:0, alpha:1000 })
+            }
         
-//     }
-// }
+    }
+}
 console.log(`Topx: ${topx}
 TopY: ${topy}
 LowX: ${lowx}
@@ -84,3 +88,83 @@ acc = data[0].reduce((pv, cv, ci, arr) => {
     //     console.log(coll)
     // })
     */
+
+
+function getTopRight(p){
+    let xy = [0,0]
+    for (let i = 0; i <p.length; i++) {
+        let ti=i;
+        for (let j = p[ti].length-1; j >= 0&&(ti>0); j--) {
+            //System.out.println(i+" "+ti+" "+j+" "+p[ti][j]);
+            if(p[ti][j]==255){
+                xy[0]=j;
+                xy[1]=ti;
+                //print(xy[0],xy[1]);
+                //System.out.println(th);
+                return xy;
+            }
+            ti--;
+        }
+    }
+
+    return xy;
+}
+function getTopLeft(p){
+    let xy = [0,0]
+    
+            for (let i = 0; i <p.length; i++) {
+                let ti=i;
+                for (let j = 0; j < p[ti].length&&(ti>0); j++) {
+                    //System.out.println(i+" "+ti+" "+j+" "+p[ti][j]);
+                    if(p[ti][j] == 255){
+                        xy[0]=j;
+                        xy[1]=ti;
+                        //print(xy[0],xy[1]);
+                        //System.out.println(th);
+                        return xy;
+                    }
+                    ti--;
+                }
+            }
+    
+            return xy;
+}
+function getBotRight(p){
+    let xy = [0,0]
+    
+            for (let i = p.length-1; i >=0; i--) {
+                let ti=i;
+                for (let j = p[ti].length-1; j >=0&&(ti<p.length-1); j--) {
+                    //System.out.println(i+" "+ti+" "+j+" "+p[ti][j]);
+                    if(p[ti][j] == 255  ){
+                        xy[0]=j;
+                        xy[1]=ti;
+                        //print(xy[0],xy[1]);
+                        //System.out.println(th);
+                        return xy;
+                    }
+                    ti++;
+                }
+            }
+    
+            return xy;
+}
+function getBotLeft(p){
+    let xy = [0,0]    
+    for (let i = p.length-1; i >=0;i--) {
+        let ti=i;
+        for (let j = 0; j < p[ti].length&&(ti<p.length-1); j++) {
+            //System.out.println(i+" "+ti+" "+j+" "+p[ti][j]);
+            if(p[ti][j] == 255){
+                xy[0]=j;
+                xy[1]=ti;
+                //print(xy[0],xy[1]);
+                //System.out.println(th);
+                return xy;
+            }
+            ti++;
+        }
+    }
+
+    return xy;
+}
